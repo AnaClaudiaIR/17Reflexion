423cdcc2d4856b98c14003ce7ae730cdba95934a
AnaClaudiaIR <anac.ihnrod@educa.jcyl.es>
2026-02-18 12:07:29 +0100

    subir2

--- src/ForEach.java
+++ src/ForEach.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class ForEach {
+public class ForEach{
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E", "F"));
+
+        System.out.println(lista);
+
+        for(String s : lista) {
+            if ("B".equals(s)) {
+                lista.remove(s); // Lanza ConcurrentModificationException
+            }
+        }
+        System.out.println(lista);
+    }
 }
--- src/Iterator.java
+++ src/Iterator.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Iterator {
+    public static   void main(String args[]){
+        List<String> lista = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E", "F"));
+        System.out.println(lista);
+
+        java.util.Iterator<String> iterator = lista.iterator();
+        while (iterator.hasNext()){
+            if(iterator.next().equals("B")){
+                iterator.remove();
+            }
+        }
+        System.out.println(lista);
+    }
 }
